4bf5cc1ab63fb5921ea2336261ccac1af2664925
Khushwant <khush123098@gmail.com>
2025-09-25 21:35:29 +0530

    db connection update

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,34 @@
-import express from 'express';
-import cors from 'cors';
-import { connectDB } from './config/db.js';
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
-// app configuration
-const app = express();
-const PORT = process.env.PORT || 4000;
+// app config
+const app = express()
+const port = 4000
 
-// middlewares
-app.use(express.json());
-app.use(cors());
+// middleware
+app.use(express.json())
+app.use(cors())
 
-// DB Connection
+// db connection
 connectDB();
 
+app.use("/api/food", foodRouter)
+app.use("/images", express.static('uploads'))
+app.use("/api/user", userRouter)
+app.use("/api/cart", cartRouter)
+app.use("/api/order", orderRouter)
+
+
 app.get("/", (req, res) => {
-    res.send("Hello from the backend!");
+    res.send("API Working")
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Server started on https://main--healthybitesbymayank.netlify.app/`)
 })
